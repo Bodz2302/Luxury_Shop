bd00952c3811188976ed60a831add4f1dc9916df
Nguyễn Đăng Khoa <23DH114390@st.huflit.edu.vn>
2024-11-15 13:22:08 +0700

    đã fix

---
+++ Luxury_Shop/Controllers/AccountListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Luxury_Shop.Models
+{
+    public class AccountListViewModel
+    {
+        public IEnumerable<User> ListAcc { get; set; }
+        public int TotalRecords { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public IEnumerable<Product> Listproduct { get; set; }
+        public IEnumerable<Category> listdanhmuc { get; set; }
+        public IEnumerable<Brand> listhuonghiru { get; set; }
+    }
+
+}
